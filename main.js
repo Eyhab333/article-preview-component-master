3bab6bf3b7c7cd64331d2c2fb6da2d3b543f7ac8
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-02-06 18:24:09 +0300

    m

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const shareIcon = document.querySelector(".share-icon");
+const tooltip = document.querySelector(".tooltip")
 
 shareIcon.addEventListener("click", () => {
-  
+  tooltip.classList.toggle("show")
 })
